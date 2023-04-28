6342e3c278393cbe87f459ddd3d74abf779f0ef5
b19cn248 <hieunm123.ptit@gmail.com>
2023-04-29 00:20:21 +0700

    kdsds

---
+++ src/main/java/com/example/explainmysql/ExplainMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.explainmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplainMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplainMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/explainmysql/ExplainMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explainmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplainMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
